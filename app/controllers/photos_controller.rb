44455baa04fa80388f48baab460105ef04d1fd72
ramirosc <ramiro@chicagobooth.edu>
2015-10-29 16:15:24 -0500

    HW CR

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,9 +5,19 @@ class PhotosController < ApplicationController
 def show
 
 @photo= Photo.find_by({:id => params[:id]})
+end
 
+def create_row
+  p = Photo.new
+  p.caption = params[:the_caption]
+  p.source = params[:the_source]
+  p.save
+
+  redirect_to("http://localhost:3000/photos")
 
 end
+
+
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
 # Routes to READ photos
   get("/photos/new", { :controller => "photos", :action => "new_form" })
-  get("/photos/create_photo", { :controller => "photos", :action => "create_row" })
+  get("/create_photo", { :controller => "photos", :action => "create_row" })
 
 
 
