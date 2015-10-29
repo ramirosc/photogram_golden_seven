8c19366be4b29041db3b4cbbc68dc5de9f5c45ee
ramirosc <ramiro@chicagobooth.edu>
2015-10-29 15:42:38 -0500

    HW C

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,6 +2,13 @@ class PhotosController < ApplicationController
   def index
     @list_of_photos = Photo.all
   end
+def show
+
+@photo= Photo.find_by({:id => params[:id]})
+
+
+end
+
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
 
+# Routes to READ photos
+  get("/photos/new", { :controller => "photos", :action => "new_form" })
+  get("/photos/create_photo", { :controller => "photos", :action => "create_row" })
+
+
+
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
